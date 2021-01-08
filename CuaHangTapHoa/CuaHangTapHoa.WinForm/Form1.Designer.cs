97a915c3c430465185c2ad63a7d2d7ecf93b9d5e
Trong Hieu <hieuntctk42@gmail.com>
2021-01-08 21:10:44 +0700

    Init commit 🚀🚀🚀

---
+++ CuaHangTapHoa/CuaHangTapHoa.WinForm/Form1.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace CuaHangTapHoa.WinForm
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.chứcNăngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.quảnLýToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.thốngKêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.groupBox5.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chứcNăngToolStripMenuItem,
+            this.tàiKhoảnToolStripMenuItem,
+            this.quảnLýToolStripMenuItem,
+            this.thốngKêToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1064, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(0, 105);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(582, 575);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Danh sách hàng hóa";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.comboBox1);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Location = new System.Drawing.Point(0, 27);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(582, 72);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Tìm kiếm nhanh hàng hóa";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.label4);
+			this.groupBox3.Controls.Add(this.label3);
+			this.groupBox3.Controls.Add(this.button4);
+			this.groupBox3.Controls.Add(this.button3);
+			this.groupBox3.Controls.Add(this.comboBox2);
+			this.groupBox3.Controls.Add(this.textBox2);
+			this.groupBox3.Location = new System.Drawing.Point(588, 27);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(476, 100);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Thông tin khách hàng";
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.dataGridView1);
+			this.groupBox4.Location = new System.Drawing.Point(588, 133);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(476, 308);
+			this.groupBox4.TabIndex = 4;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Danh sách sản phẩm đã chọn";
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Location = new System.Drawing.Point(588, 631);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(476, 49);
+			this.panel1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(168, 43);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Thanh toán";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(296, 3);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(168, 43);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Hủy";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Controls.Add(this.panel3);
+			this.groupBox5.Controls.Add(this.panel2);
+			this.groupBox5.Location = new System.Drawing.Point(588, 447);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(479, 178);
+			this.groupBox5.TabIndex = 5;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "Chi tiết đơn hàng";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+			this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(3, 24);
+			this.dataGridView1.MultiSelect = false;
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dataGridView1.Size = new System.Drawing.Size(470, 281);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(340, 27);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(236, 28);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(236, 28);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(143, 27);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(218, 28);
+			this.textBox2.TabIndex = 2;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(143, 62);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(327, 28);
+			this.comboBox2.TabIndex = 2;
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+			this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(413, 23);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(32, 32);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+			this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Location = new System.Drawing.Point(370, 24);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(32, 32);
+			this.button4.TabIndex = 3;
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Tổng tiền:";
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.textBox3);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(6, 27);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(464, 38);
+			this.panel2.TabIndex = 2;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.Color.White;
+			this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox3.Location = new System.Drawing.Point(196, 8);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ReadOnly = true;
+			this.textBox3.Size = new System.Drawing.Size(252, 21);
+			this.textBox3.TabIndex = 1;
+			this.textBox3.Text = "120.000";
+			this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.textBox4);
+			this.panel3.Controls.Add(this.label2);
+			this.panel3.Location = new System.Drawing.Point(6, 71);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(467, 98);
+			this.panel3.TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.Color.White;
+			this.textBox4.Location = new System.Drawing.Point(89, 9);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(369, 80);
+			this.textBox4.TabIndex = 1;
+			this.textBox4.Text = "Đây là ghi chú";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(66, 20);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Ghi chú:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 65);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(121, 20);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Tên khách hàng:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 29);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(125, 20);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Tìm kiếm nhanh:";
+			// 
+			// chứcNăngToolStripMenuItem
+			// 
+			this.chứcNăngToolStripMenuItem.Name = "chứcNăngToolStripMenuItem";
+			this.chứcNăngToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+			this.chứcNăngToolStripMenuItem.Text = "Chức năng";
+			// 
+			// tàiKhoảnToolStripMenuItem
+			// 
+			this.tàiKhoảnToolStripMenuItem.Name = "tàiKhoảnToolStripMenuItem";
+			this.tàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+			this.tàiKhoảnToolStripMenuItem.Text = "Tài khoản";
+			// 
+			// quảnLýToolStripMenuItem
+			// 
+			this.quảnLýToolStripMenuItem.Name = "quảnLýToolStripMenuItem";
+			this.quảnLýToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+			this.quảnLýToolStripMenuItem.Text = "Quản lý hàng hóa";
+			// 
+			// thốngKêToolStripMenuItem
+			// 
+			this.thốngKêToolStripMenuItem.Name = "thốngKêToolStripMenuItem";
+			this.thốngKêToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+			this.thốngKêToolStripMenuItem.Text = "Thống kê";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(1064, 681);
+			this.Controls.Add(this.groupBox5);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Quản lý cửa hàng tạp hóa";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.groupBox3.PerformLayout();
+			this.groupBox4.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.groupBox5.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox5;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ToolStripMenuItem chứcNăngToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem tàiKhoảnToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem quảnLýToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem thốngKêToolStripMenuItem;
+	}
+}
+
